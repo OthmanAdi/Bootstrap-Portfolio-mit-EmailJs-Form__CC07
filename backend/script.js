9a33527bd18fc82b979ab4db55b9073a3ed9dba3
adi <adiatwork@outlook.com>
2023-02-27 14:00:28 +0100

    Contact Form v.1

--- backend/script.js
+++ backend/script.js
@@ -4,9 +4,19 @@ let UL = document.querySelector(".myUl");
 
 button.addEventListener("click", () => {
     let extractedText = document.querySelector(".form-control").value;
-    // alert(extractedText);
-    let TextNode = document.createTextNode(extractedText);
-    let listItem = document.createElement("li");
-    listItem.appendChild(TextNode);
-    UL.appendChild(listItem);
+    if (extractedText == "") {
+        alert("Empty field not Allowed");
+    }
+    else {
+        // alert(extractedText);
+        let TextNode = document.createTextNode(extractedText);
+        let listItem = document.createElement("li");
+        listItem.appendChild(TextNode);
+
+        listItem.classList.add("noDot");
+
+        UL.appendChild(listItem);
+
+        document.querySelector(".form-control").value = "";
+    }
 });
