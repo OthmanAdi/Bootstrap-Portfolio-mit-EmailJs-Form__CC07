53f107834e6cfa5816b3b5213ce8b4cbb43c8c6b
adi <adiatwork@outlook.com>
2023-03-02 16:46:19 +0100

    ⚡️✨ Implemented: Working contact form, Bootstrap Cards, Anchor Animation, Api Playground

--- backend/script.js
+++ backend/script.js
@@ -1,36 +1,53 @@
-let button = document.querySelector(".btn");
-// let UL = document.querySelector(".myUl");
+let button = document.querySelector(".btn"); //Todolist Submit button
+let UL = document.querySelector(".myUl"); //Ul in Todolist html
 
 // Contact Form
 let contactForm = document.querySelector(".contact-form");
 
+// ContactForm algo
 window.onload = function () {
     contactForm.addEventListener("submit", (e) => {
         e.preventDefault();
         emailjs.sendForm("service_7mx8nld", "template_epdqyxp", e.target)
             .then(function () {
-                alert("HORRAAYYYY 🌟🌟🌟✨✨✨ You sent the Form!!!");
+                Swal.fire({
+                    title: "Horray!, You sent us a Form",
+                    text: "We'll be back to you ASAP",
+                    icon: "success",
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: "OK"
+                }).then(() => {
+                    contactForm.reset();
+                });
             }, function (error) {
-                alert(error);
-            })
+                Swal.fire({
+                    title: "Oops...",
+                    text: "Something went Wrong! Please try again later",
+                    icon: "error",
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: "OK"
+                });
+            });
     });
-}
+};
 
-// button.addEventListener("click", () => {
-//     let extractedText = document.querySelector(".form-control").value;
-//     if (extractedText == "") {
-//         alert("Empty field not Allowed");
-//     }
-//     else {
-//         // alert(extractedText);
-//         let TextNode = document.createTextNode(extractedText);
-//         let listItem = document.createElement("li");
-//         listItem.appendChild(TextNode);
+// Todolist algo
+button.addEventListener("click", () => {
+    let extractedText = document.querySelector(".form-control").value;
+    if (extractedText == "") {
+        alert("Empty field not Allowed");
+    }
+    else {
+        // alert(extractedText);
+        let TextNode = document.createTextNode(extractedText);
+        let listItem = document.createElement("li");
+        listItem.appendChild(TextNode);
 
-//         listItem.classList.add("noDot");
+        listItem.classList.add("noDot");
 
-//         UL.appendChild(listItem);
+        UL.appendChild(listItem);
 
-//         document.querySelector(".form-control").value = "";
-//     }
-// });
+        document.querySelector(".form-control").value = "";
+    }
+});
+
